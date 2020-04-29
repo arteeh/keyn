065f7c5f39da2a9b917f18af239f08530c8bf3ed
Maarten <me@arteeh.com>
2020-04-29 19:12:30 +0200

    debugging

--- top-cache.php
+++ top-cache.php
@@ -6,16 +6,25 @@ $file = $break[count($break) - 1];
 $cachefile = 'cache/cached-'.substr_replace($file ,"",-4).'.php';
 $cachetime = 18000;
 
+echo "url: $url<br>";
+foreach($break as $i)
+	echo "break: $i<br>";
+echo "<br>file: $file<br>";
+echo "cachefile: $cachefile<br>";
+echo "cachetime: $cachetime<br><br>";
+echo "if (file_exists($cachefile) && time() - $cachetime < filemtime($cachefile))<br>";
+echo "if (" . file_exists($cachefile) . " && " . time() . " - $cachetime " . " < " . filemtime($cachefile) . ")<br>";
+
 // Serve from the cache if it is younger than $cachetime
 if (file_exists($cachefile) && time() - $cachetime < filemtime($cachefile))
 {
-	echo "<!-- Cached copy, generated ".date('H:i', filemtime($cachefile))." -->\n";
+	echo "Cached copy, generated " . date('H:i', filemtime($cachefile)). "<br>";
 	readfile($cachefile);
 	exit;
 }
 else
 {
-	echo "<!-- Not cached. File: $file -->\n";
+	echo "Not cached. File: $file<br>";
 }
 
 ob_start(); // Start the output buffer
