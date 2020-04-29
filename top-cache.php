1bd8b7490acd8bef2ebe680cf3db93dcf8a1ba15
Maarten <me@arteeh.com>
2020-04-29 15:09:18 +0200

    not in cache folder for now

--- top-cache.php
+++ top-cache.php
@@ -2,7 +2,7 @@
 $url = $_SERVER["SCRIPT_NAME"];
 $break = Explode('/', $url);
 $file = $break[count($break) - 1];
-$cachefile = 'cache/cached-'.substr_replace($file ,"",-4).'.html';
+$cachefile = 'cached-'.substr_replace($file ,"",-4).'.php';
 $cachetime = 18000;
 
 // Serve from the cache if it is younger than $cachetime
@@ -14,7 +14,7 @@ if (file_exists($cachefile) && time() - $cachetime < filemtime($cachefile))
 }
 else
 {
-	echo "<!-- Not cached. File: $file, Break: $break -->\n";
+	echo "<!-- Not cached. File: $file -->\n";
 }
 
 ob_start(); // Start the output buffer
