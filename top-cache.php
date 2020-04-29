3c9f5630c818b107b876f5a40e22b777ff518f8e
Maarten <me@arteeh.com>
2020-04-29 20:35:00 +0200

    don't cache register page

--- top-cache.php
+++ top-cache.php
@@ -2,34 +2,38 @@
 $url = $_SERVER["SCRIPT_NAME"];
 $break = Explode('/', $url);
 $file = $break[count($break) - 1];
-$cachefile = 'cache/cached-' . substr_replace($file ,"",-4);
+if(	$file == "register.php"	||
+	$file == "login.php"	)
+	$dontcache = 1;
 
-$questionmarked = 0;
-foreach($_GET as $key => $value)
+if(!$dontcache)
 {
-	if(!$questionmarked)
+	$cachefile = 'cache/cached-' . substr_replace($file ,"",-4);
+	$questionmarked = 0;
+	foreach($_GET as $key => $value)
 	{
-		$cachefile = $cachefile . "_where_$key" . "_is_" . "$value";
-		$questionmarked++;
+		if(!$questionmarked)
+		{
+			$cachefile = $cachefile . "_where_$key" . "_is_" . "$value";
+			$questionmarked++;
+		}
+		else
+			$cachefile = $cachefile . "_and_$key" . "_is_" . "$value";
 	}
-	else
-		$cachefile = $cachefile . "_and_$key" . "_is_" . "$value";
+	$cachetimeminutes = 10;
+	$cachetime = $cachetimeminutes * 60;
+	// Serve from the cache if it is younger than $cachetime
+	if (file_exists($cachefile))
+	{
+		if(time() - $cachetime < filemtime($cachefile))
+		{
+			echo "<!-- Cached page, generated at " . date('H:i', filemtime($cachefile)). " -->";
+			readfile($cachefile);
+			exit;
+		}
+	}
+	ob_start(); // Start the output buffer
 }
 
-$cachetimeminutes = 10;
-$cachetime = $cachetimeminutes * 60;
-
-// Serve from the cache if it is younger than $cachetime
-if (file_exists($cachefile))
-{
-	if(time() - $cachetime < filemtime($cachefile))
-	{
-		echo "<!-- Cached page, generated at " . date('H:i', filemtime($cachefile)). " -->";
-		readfile($cachefile);
-		exit;
-	}
-}
-
-ob_start(); // Start the output buffer
 
 ?>
