cff7e867c278f2647e37d6ea1c60d9a8900f3065
Maarten de Jong <maarten.de.jong@batenburg.nl>
2024-11-14 17:35:21 +0100

    start with mudblazor template

--- Keyn/Program.cs
+++ Keyn/Program.cs
@@ -1,13 +1,25 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Keyn;
+using MudBlazor.Services;
+using Keyn.Components;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// Add MudBlazor services
+builder.Services.AddMudServices();
 
-builder.Services.AddBlazorBootstrap();
+// Add services to the container.
+builder.Services.AddRazorComponents();
 
-await builder.Build().RunAsync();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>();
+
+app.Run();
